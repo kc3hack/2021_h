0f610a17b50d79a39306ce32f991bb0109b4c533
Kotokaze <5r1j6lvag@relay.firefox.com>
2021-03-14 23:50:00 +0900

    update

--- webapp/app.js
+++ webapp/app.js
@@ -3,11 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
+var session = require('express-session');
+var GitHubStrategy = require('passport-github2').Strategy;
+var passport = require('passport');
+
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+passport.serializeUser((user, cb) => { cb(null, user); });
+passport.deserializeUser((obj, cb) => { cb(null, obj); });
+passport.use(new GitHubStrategy(
+  {
+    clientID: config.GitHub.clientID,
+    clientSecret: config.GitHub.clientSecret,
+    callbackURL: config.GitHub.callbackURL
+  },
+
+  function (accessToken, refreshToken, profile, done) {
+
+    process.nextTick(() => done(null, profile));
+  }
+));
+
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +41,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: config.session.secret, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  (req, res) => { }
+);
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => res.redirect('/')
+);
+app.get('/login', (req, res) => res.render('login'));
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- webapp/config.js
+++ webapp/config.js
@@ -1,16 +1,12 @@
 module.exports = {
 
 	GitHub: {
-		clientID: GITHUB_CLIENT_ID,
-		clientSecret: GITHUB_CLIENT_SECRET,
+		clientID: '$GITHUB_CLIENT_ID',
+		clientSecret: '$GITHUB_CLIENT_SECRET',
 		callbackURL: 'http://localhost:8000/auth/github/callback'
 	},
 
-	twitter: {
-		consumerKey: '${API key}',
-		consumerSecret: '${API secret key}',
-		callbackURL: 'http://localhost:8000/oauth_callback'
-	},
-
-	ipAddress: 'http://localhost:8000'
+	session: {
+		secret: '336d2ebdb0acde42'
+	}
 }
