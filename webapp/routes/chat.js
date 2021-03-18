7b7a99267762a7924c196a281223bc92aa9c6c89
konchanksu <nyankomi59@gmail.com>
2021-03-18 19:58:54 +0900

    データベースわからん

--- webapp/routes/chat.js
+++ webapp/routes/chat.js
@@ -8,9 +8,9 @@ router.get('/', authenticationEnsurer, (req, res, next) => {
 	res.render('chat', { user: req.user, card: Card });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', authenticationEnsurer, (req, res, next) => {
 	console.log(req.body['content']);
-	res.render('chat', { user: req.user, card: Card });
+	res.render('chat', { user: req.user, card: req.body['content'] });
 })
 
 module.exports = router;
