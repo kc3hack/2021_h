766aa04ee4d5a045dd4578c47e1a5abfdf32a15c
SHND70 <n.shindoh.1999@gmail.com>
2021-03-18 23:43:23 +0900

    mermaid.js動作確認済み

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'プロダクト名', user: req.user });
   res.render('index', { title: 'プロダクト名', user: req.user });
 });
 
