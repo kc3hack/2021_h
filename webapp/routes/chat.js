42dca4e303df86f60144295f59325059f55d276a
Kotokaze <5r1j6lvag@relay.firefox.com>
2021-03-20 01:11:43 +0900

    implement: display chat data

--- webapp/routes/chat.js
+++ webapp/routes/chat.js
@@ -4,26 +4,37 @@ const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
 const User = require('../models/user');
 const Card = require('../models/card');
+const moment = require('moment-timezone');
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
 
 router.get('/', authenticationEnsurer, csrfProtection, (req, res, next) => {
 
-	res.render('chat', { title: 'Chatree - OpenChat', user: req.user, csrfToken: req.csrfToken() });
+  if (req.user) {
+
+    Card.findAll( { order: [['updatedAt', 'DESC']] }
+    ).then((cards) => {
+
+      cards.forEach((card) => card.formattedUpdatedAt = moment(card.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm'));
+      res.render('chat', { title: 'Chatree - OpenChat', user: req.user, cards: cards, csrfToken: req.csrfToken() });
+    });
+  }
+
+  else res.render('chat', { title: 'Chatree - OpenChat', user: req.user, csrfToken: req.csrfToken() });
 });
 
 router.post('/', authenticationEnsurer, csrfProtection, (req, res, next) => {
 
-	const updatedAt = new Date();
-	// console.log(req.body);
-	Card.create(
-		{
-			sendTo: parseInt(req.body.sendTo) || null,
-			content: req.body.content.slice(0, 255),
-			createdBy: req.user.id,
-			updatedAt: updatedAt,
-		}
-	).then(() => res.redirect('/chat'));
+  const updatedAt = new Date();
+  // console.log(req.body);
+  Card.create(
+    {
+      sendTo: parseInt(req.body.sendTo) || null,
+      content: req.body.content.slice(0, 255),
+      createdBy: req.user.id,
+      updatedAt: updatedAt,
+    }
+  ).then(() => res.redirect('/chat'));
 })
 
 module.exports = router;
