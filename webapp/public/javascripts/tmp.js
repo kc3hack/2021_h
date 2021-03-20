ae160431df22bd0b501ef9e5cad8863655b160db
SHND70 <n.shindoh.1999@gmail.com>
2021-03-20 17:16:45 +0900

    ログイン画面とプロフィール画面に変更を加えました

--- webapp/public/javascripts/tmp.js
+++ webapp/public/javascripts/tmp.js
@@ -1,34 +1,34 @@
-(function () {
+(function() {
 	function drawLine() {
-		var canvas = document.getElementById("myCanvas");
+	  var canvas = document.getElementById("myCanvas");
 
-		if (!canvas || !canvas.getContext) {
-			return false;
-		}
+	  if (!canvas || !canvas.getContext) {
+		return false;
+	  }
 
-		var ctx = canvas.getContext("2d");
+	  var ctx = canvas.getContext("2d");
 
-		var a = document.getElementById("doko").getBoundingClientRect();
-		var b = (a.bottom - a.top) / 2;
-		var c = (a.right - a.left) / 2;
-		var d = a.top + b;
-		var e = a.left + c;
+	  var a = document.getElementById("doko").getBoundingClientRect();
+	  var b = (a.bottom - a.top) /2;
+	  var c = (a.right - a.left) /2;
+	  var d = a.top+b;
+	  var e = a.left+c;
 
-		var kokoda = document.getElementById("koko").getBoundingClientRect();
-		var tate = (kokoda.bottom - kokoda.top) / 2;
-		var yoko = (kokoda.right - kokoda.left) / 2;
-		var tatehalf = kokoda.top + tate;
-		var yokohalf = kokoda.left + yoko;
+	  var kokoda = document.getElementById("koko").getBoundingClientRect();
+	  var tate = (kokoda.bottom - kokoda.top)/2;
+	  var yoko = (kokoda.right - kokoda.left)/2;
+	  var tatehalf =kokoda.top+tate;
+	  var yokohalf =kokoda.left+yoko;
 
-		ctx.strokeStyle = "#ffba00";
-		ctx.lineWidth = 10;
-		ctx.beginPath();
-		ctx.moveTo(d, e);
-		ctx.lineTo(yokohalf, tatehalf);
-		ctx.setLineDash([5, 10]);
-		ctx.closePath();
-		ctx.stroke();
+	  ctx.strokeStyle = "#ffba00";
+	  ctx.lineWidth = 5;
+	  ctx.beginPath();
+	  ctx.moveTo(d,e);
+	  ctx.lineTo(yokohalf,tatehalf);
+	  ctx.setLineDash([5, 10]);
+	  ctx.closePath();
+	  ctx.stroke();
 	}
 
 	onload = drawLine;
-})();
+  })();
--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -4,13 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-<<<<<<< HEAD
-  res.render('test', { title: 'Chatree', user: req.user });
+  res.render('index', { title: 'Chatree', user: req.user });
   // res.render('test', { title: 'Chatree', user: req.user });
-=======
-  // res.render('index', { title: 'Chatree', user: req.user });
-  res.render('test', { title: 'Chatree', user: req.user });
->>>>>>> 6e9ef1227f7cc9f25c35579af5ce75298c4c1dd0
 });
 
 module.exports = router;
