3152329e7f07e21070b40622201d93ae5fba6444
SHND70 <n.shindoh.1999@gmail.com>
2021-03-20 23:55:33 +0900

    デモ用のブランチです

--- webapp/app/entry.js
+++ webapp/app/entry.js
@@ -4,12 +4,23 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import Two from 'two.js';
 import $ from 'jquery';
+import mermaid from 'mermaid';
 
 // loads the Icon plugin
 UIkit.use(Icons);
 
 // components can be called from the imported UIkit reference
 // UIkit.notification('Hello world.');
+
+$(document).ready(() => {
+  mermaid.initialize(
+    {
+      theme: 'forest',
+      startOnLoad: true,
+      securityLevel: 'antiscript'
+    }
+  );
+});
 
 $(function () {
 
