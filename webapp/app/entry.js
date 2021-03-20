b710bd491c451ddb13d01d742a995468eda41dfc
konchanksu <nyankomi59@gmail.com>
2021-03-20 13:20:38 +0900

    test

--- webapp/app/entry.js
+++ webapp/app/entry.js
@@ -4,6 +4,7 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 import mermaid from 'mermaid';
+import Two from 'two.js'
 
 $(document).ready(() => {
   mermaid.initialize(
@@ -13,6 +14,29 @@ $(document).ready(() => {
       securityLevel: 'antiscript'
     }
   );
+
+  var elem = $(document).getElementByClassName('draw-group');
+  var two = new Two({ width: 285, height: 200 }).appendTo(elem);
+
+  var circle = two.makeCircle(-70, 0, 50);
+  var rect = two.makeRectangle(70, 0, 100, 100);
+  circle.fill = '#FF8000';
+  circle.stroke = 'orangered';
+  rect.fill = 'rgba(0, 200, 255, 0.75)';
+  rect.stroke = '#1C75BC';
+
+  // Groups can take an array of shapes and/or groups.
+  var group = two.makeGroup(circle, rect);
+
+  // And have translation, rotation, scale like all shapes.
+  group.translation.set(two.width / 2, two.height / 2);
+  group.rotation = Math.PI;
+  group.scale = 0.75;
+
+  // You can also set the same properties a shape have.
+  group.linewidth = 7;
+
+  two.update();
 });
 
 
